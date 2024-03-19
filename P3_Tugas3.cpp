c489457c63b7864088f4b864835bc3ff19542d85
tacosz <nabilnasruddin.offc@gmail.com>
2024-03-19 18:46:54 +0700

    Perulangan Do While

---
+++ P3_Tugas3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int bilangan;
+    srand(time(0));
+    bilangan = rand() % 10;
+    cout << "Bilangan Awal: " << bilangan << endl;
+
+    do
+    {
+        cout << "Peluang DO...WHILE" << endl;
+        bilangan = rand() % 10;
+        cout << "Bilangannya= " << bilangan << endl;
+    } 
+    while (bilangan > 5);
+}
